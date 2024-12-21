5fe252e17f9cd740d7b7d6de888815f940cc7bb3
NkululekoNtuli <scelonkululeko2@gmail.com>
2024-12-21 22:47:32 +0200

    served the HTLM page

--- myApp.js
+++ myApp.js
@@ -3,10 +3,14 @@ let app = express();
 
 // console.log("Hello World");
 
-app.get("/", function(rep, res){
-    res.send("Hello Express")
-});
+// app.get("/", function(rep, res){
+//     res.send("Hello Express")
+// });
 
+app.get ("/",  function(rep, res){
+    absolutePath = __dirname + "/views/index.html";
+    res.send(absolutePath);
+});
 
 
 
